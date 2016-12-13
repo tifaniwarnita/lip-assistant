10137425083b334cc25f0b8514e1a55f0c611ed9
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-12-13 16:45:32 +0700

    Resolve conflicts

--- src/LipAssistant.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Tifani on 12/13/2016.
- */
-public class LipAssistant {
-
-    public static void main(String args[]){
-
-    }
-}
---
+++ src/miaw.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Tifani on 12/13/2016.
+ */
+public class miaw {
+}
